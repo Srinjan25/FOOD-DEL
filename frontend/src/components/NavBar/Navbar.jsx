aebbb3dfd5b6285f3ada5556de3da12705f75d33
Srinjan Tambuli <sr8672@yahoo.com>
2025-08-26 15:55:21 +0530

    Update Navbar.jsx

--- frontend/src/components/NavBar/Navbar.jsx
+++ frontend/src/components/NavBar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import './Navbar.css'
+import './NavBar.css'
 import { assets } from '../../assets/assets'
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
